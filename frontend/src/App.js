c43047c63926d34a476424585959fc245579edd3
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-12-04 11:03:54 +0100

    refactor project

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Connexion from "./Login/Login"
+
+function App() {
+  return (
+    <div className="App">
+      <Connexion/>
+    </div>
+  );
+}
+
+export default App;
